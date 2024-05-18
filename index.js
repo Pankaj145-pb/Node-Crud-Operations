596791d871ce622a518e1178e9e1481540d50f56
pankaj145-pb <pankaj.ab.net@gmail.com>
2024-05-19 00:29:11 +0530

    "Added db connection file and updated index.js to require and call it; updated .gitignore to ignore db directory"

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
-const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
+const connectDB = require('./db/db.connection.js')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
-const mongoose = require("mongoose");
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+//Mongo Database Connection
+connectDB();
 
 //routes
 app.use("/api/products", productRoute);
@@ -19,4 +20,3 @@ app.get("/", (req, res) => {
   res.send("Hello from node apis");
 });
 
-//Database Connection
